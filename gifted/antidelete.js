0ed198b6e0210d443b32a711158929902e0b3a04
OFFICIAL GURU <hapawapi0@gmail.com>
2025-12-23 12:16:42 +0300

    X GURU

--- gifted/antidelete.js
+++ gifted/antidelete.js
@@ -2,25 +2,18 @@ const { evt } = require("../gift");
 
 evt({
   pattern: "antidelete",
-  desc: "Enable or disable anti delete messages",
+  desc: "Enable/disable anti delete",
   category: "owner"
 }, async (Gifted, m, { reply, isSuperUser, config, args }) => {
 
-  if (!isSuperUser) return reply("❌ Owner only command");
+  if (!isSuperUser) return reply("❌ Owner only");
 
-  if (!args[0]) {
-    return reply(
-      `♻️ Anti Delete: *${config.ANTIDELETE}*\n\nOptions:\ntrue\nfalse\ninboxonly\nchatsonly`
-    );
-  }
+  if (!args[0]) return reply(`♻️ Anti Delete: *${config.ANTIDELETE}*\nOptions: inbox/chats/off`);
 
   const value = args[0].toLowerCase();
-  const allowed = ["true", "false", "inboxonly", "chatsonly"];
-
-  if (!allowed.includes(value)) {
-    return reply("❌ Invalid option");
-  }
+  if (!["inbox", "chats", "off"].includes(value)) return reply("❌ Invalid option");
 
   config.ANTIDELETE = value;
+
   reply(`✅ Anti Delete set to *${value}*`);
 });
