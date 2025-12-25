24069a02e71e290d4d5168817f61ab1784da6bb6
OFFICIAL GURU <hapawapi0@gmail.com>
2025-12-26 01:09:20 +0300

    X GURU

--- gift/gmdFunctions.js
+++ gift/gmdFunctions.js
@@ -364,8 +364,8 @@ async function loadSession() {
 
         const [header, b64data] = config.SESSION_ID.split('~');
 
-        if (header !== "Gifted" || !b64data) {
-            throw new Error("❌ Invalid session format. Expected 'Gifted~.....'");
+        if (header !== "Xguru" || !b64data) {
+            throw new Error("❌ Invalid session format. Expected 'Xguru~.....'");
         }
 
         const cleanB64 = b64data.replace('...', '');
